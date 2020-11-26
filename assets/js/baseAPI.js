590bca004688a1cb1bf476b8bc15331a88395e19
tan <2274562300@qq.com>
2020-11-26 10:37:48 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,27 @@
 //开发环境服务器地址
+//拦截所有的Ajax请求：get/post/ajax
 var baseURL = "http://ajax.frontend.itheima.net";
+// 处理函数
 $.ajaxPrefilter(function(params){
     //拼接对应环境的服务器地址
     params.url = baseURL + params.url;
+    //alert（params.url）
+    //对需要权限的接口配置头信息
+    //必须没有开头才行
+    if(params.url.indexOf("/my/") !== -1){
+        params.headers = {
+            Authorization:localStorage.getItem("token") || "",
+        }
+    }
+    //拦截所有响应，判断身份认证信息
+    params.complete = function(res){
+        console.log(res.responseJSON)
+        var obj = res.responseJSON;
+        if(obj.status == 1 && obj.message == '身份认证失败！'){
+            //1.清空本地token
+            localStorage.removeItem("token");
+            //页面跳转
+            location.href = "/login.html"
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function(){
+    //获取有用信息
+    getUserInof();
+    //退出功能
+    var layer = layui.layer;
+    $("#btnLogout").on("click",function(){
+        //框架提供的询问狂
+        layer.confirm("是否确认退出?",{ icon: 3, title: "提示" },function(index){
+            //清空本地token
+        localStorage.removeItem("token");
+        location.href="/login.html";
+        //关闭询问闯
+        layer.close(index);
+        })
+    })
+})
+function getUserInof(){
+    $.ajax({
+        url:'/my/userinfo',
+        // headers:{
+        //     //重新登陆，因为token过期事件12小时
+        //     Authorization:localStorage.getItem("token") || "",
+
+        // },
+        success:function (res) {
+            //判断状态码
+            console.log(res);
+            if(res.status !== 0){
+                return layui.layer.msg(res.message);
+            }
+            //请求成功，渲染用户头信息
+            renderAvatar(res.data);
+        }
+    })
+}
+//封装用户名头像渲染函数
+function renderAvatar(user){
+    //1.用户名 昵称优先，没有用username
+    var name = user.nickname || user.username;
+    $("#welcome").html("欢迎  "+name);
+    //用户头像
+    if(user.user_pic !== null){
+        //有头像
+        $(".layui-nav-img").show().attr("src",user.user_pic);
+        $(".text-avatar").hide();
+    }else{
+        //没有头像
+        $(".layui-nav-img").hide();
+        var text = name[0].toUpperCase();//首字母大写
+        $(".text-avatar").show().html(text);
+    }
+}
